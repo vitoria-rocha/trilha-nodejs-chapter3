2235986bf4e16d2b6d92179d9e8d73ebebe644fd
vitoria-rocha <vitoria.rocha1206@gmail.com>
2021-10-13 14:10:06 -0400

    --Inserindo ID com uuid

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,6 @@ const app = express();
 
 app.use(express.json());
 
-app.use(categoriesRoutes);
+app.use("/categories", categoriesRoutes);
 
 app.listen(8080, () => console.log("server is running: http://localhost:8080"));
