fce5c1a4952dcd3124c8bb1a98a87e1d3b485ea6
vitoria-rocha <vitoria.rocha1206@gmail.com>
2021-10-13 14:02:26 -0400

    --Criando categoria

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,10 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
-
 app.use(express.json());
-app.get("/", (request, response) => {
-  return response.json({message: "oi"});
-});
 
-app.post("/courses", (request, response) =>{
-  const { name } = request.body;
-  return response.json({ name });
-});
+app.use(categoriesRoutes);
 
 app.listen(8080, () => console.log("server is running: http://localhost:8080"));
