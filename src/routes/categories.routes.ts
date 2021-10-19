05075cc64602663e9fd6294c9a368675aaf4e7f2
vitoria-rocha <vitoria.rocha1206@gmail.com>
2021-10-19 14:18:34 -0400

    --Criando use case

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/model/useCases/createCategory";
+import { ImportCategoryController } from "../modules/cars/model/useCases/importCategory/ImportCategoryController";
 import { listCategoriesController } from "../modules/cars/model/useCases/listCategories";
 
 const categoriesRoutes = Router();
@@ -19,9 +20,7 @@ categoriesRoutes.get("/", (request, response) => {
 });
 
 categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
-  const { file } = request;
-  console.log(file);
-  return response.send();
+  return ImportCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
