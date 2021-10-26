ef3abc91ceadbcd562f597fbe80f5fdee984d618
vitoria-rocha <vitoria.rocha1206@gmail.com>
2021-10-26 15:23:08 -0400

    --Criando migration de especificaçao

--- src/modules/cars/entities/Specification.ts
+++ src/modules/cars/entities/Specification.ts
@@ -1,12 +1,18 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn} from "typeorm";
 import { v4 as uuidv4 } from "uuid";
 
+@Entity("specifications")
 class Specification {
+  @PrimaryColumn()
   id?: string;
 
+  @Column()
   name: string;
 
-  descriprion: string;
+  @Column()
+  description: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
 
--- src/modules/cars/entities/repositories/implementations/SpecificationRepository.ts
+++ src/modules/cars/entities/repositories/implementations/SpecificationRepository.ts
@@ -1,31 +1,29 @@
+import { getRepository, Repository } from "typeorm";
 import { Specification } from "../../Specification";
 import { ICreateSpecificationDTO, ISpecificationRepository } from "../ISpecificationRepository";
 
 
 class SpecificationRepository implements ISpecificationRepository{
-  private specifications: Specification[];
+  private repository: Repository<Specification>;
 
   constructor(){
-    this.specifications = [];
+    this.repository = getRepository(Specification);
   }
   
-  create({ description, name }: ICreateSpecificationDTO): void {
-    const specification = new Specification();
-
-    Object.assign(specification, {
-      name,
+  async create({ description, name }: ICreateSpecificationDTO): Promise<void> {
+    const specification = this.repository.create({
       description,
-      created_at: new Date(),
+      name,
     });
 
-    this.specifications.push(specification);
+    await this.repository.save(specification);
   }
 
-  findByName(name: string): Specification{
-    const specification = this.specifications.find(
-      (specification) => specification.name === name
-      );
-      return specification;
+  async findByName(name: string): Promise<Specification>{
+    const specification = this.repository.findOne({
+      name,
+    });
+    return specification;
   }
 
 }
