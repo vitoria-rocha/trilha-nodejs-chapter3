be6c383ad188473985241a58b7bc859b2ebcd0fc
vitoria-rocha <vitoria.rocha1206@gmail.com>
2021-10-28 10:20:20 -0400

    --Criando controller de usuario

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,15 +1,16 @@
-
+import { Request, Response } from "express";
 import { inject, injectable } from "tsyringe";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUserRepository } from "../../repositories/IUserRepository";
 
+@injectable()
 class CreateUserUseCase {
   constructor(
     @inject("UsersRepository")
     private usersRepository: IUserRepository
   ){}
 
- async execute({ name, username, email, password, driver_license }: ICreateUserDTO): Promise<void>{
+  async execute({ name, username, email, password, driver_license }: ICreateUserDTO): Promise<void>{
     await this.usersRepository.create({
       name,
       username, 
